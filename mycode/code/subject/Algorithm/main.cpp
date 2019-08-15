95897aa84be41f0be7b6349b3aed960261d6c719
Jinxin-Zhao <877290840@qq.com>
2019-08-15 18:14:00 +0800

    Update main.cpp

--- mycode/code/subject/Algorithm/main.cpp
+++ mycode/code/subject/Algorithm/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Fibonacci.h"
+#include "Quickselect.h"
 
 int main() {
     //test Fibonacci
@@ -8,7 +9,19 @@ int main() {
     std::cout<<"f(n): "<<res_n<<std::endl;
 
     //test matrix method
-    int res = fibonacci.getFibnacci_matrix(6);
-    std::cout<<"f(n): "<<res<<std::endl;
+    int matrix_res = fibonacci.getFibnacci_matrix(14);
+    std::cout<<"matrix result: "<<matrix_res<<std::endl;
+    //
+    //srand(unsigned(time(NULL)));
+    //for (int j = 0; j < num_array; j++)
+    //array[j] = rand();
+    // 寻找第k最小数
+    Quickselect q;
+    int k = 13;
+    int i = q.q_select(array, 0, num_array - 1, k);
+    std::cout <<"kth num is : "<< i << std::endl;
+    for(int j = 0; j < num_array; j++){
+        std::cout<<array[j]<<" ";
+    }
     return 0;
 }
